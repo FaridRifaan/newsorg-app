51ef6b7aaad23b0f3dd9b5c1427f2316e3f4d19b
Farid Rif'an <100013881+FaridRifaan@users.noreply.github.com>
2025-02-13 21:20:16 +0700

    Update NewsResponse.kt

--- app/src/main/java/com/project/newsorg_app/models/NewsResponse.kt
+++ app/src/main/java/com/project/newsorg_app/models/NewsResponse.kt
@@ -4,11 +4,11 @@ import com.google.gson.annotations.SerializedName
 
 data class NewsResponse(
     @SerializedName("status")
-    val status: String,  // Status response (misal: "ok")
+    val status: String, 
 
     @SerializedName("totalResults")
-    val totalResults: Int,  // Total berita yang tersedia
+    val totalResults: Int, 
 
     @SerializedName("articles")
-    val articles: List<Article>  // List artikel berita
+    val articles: List<Article>  
 )
